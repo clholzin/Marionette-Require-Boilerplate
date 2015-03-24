95cfc8dd6dc6cc29d66616ab11d5c98e4a92fc67
clholzin <clholzin@yahoo.com>
2015-03-24 14:15:17 -0700

    added script - ch

--- public/js/app/models/RegisterModel.js
+++ public/js/app/models/RegisterModel.js
@@ -3,7 +3,7 @@ define(["App","jquery", "backbone"],
         // Creates a new Backbone Model class object
        App.RegisterModel = Backbone.Model.extend({
             idAttribute: "_id",
-            urlRoot:'http://localhost:8001/register',
+            urlRoot:'http://192.68.2.98:8001/register',
             // Model Constructor
             initialize: function() {
                 this.on('error', this.showError);
